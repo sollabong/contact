3f4bc946b28dfc97a3f6d9bef7076a5c18a06df2
Gabor Soltesz-Keri <61150302+sollabong@users.noreply.github.com>
2021-02-04 15:49:29 +0100

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,24 @@
+<?php
+
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
+
+$email_from = $visitor_email;
+$email_subject = "New form submission";
+$email_body = "User Name: $name.\n". 
+"User Email: $visitor_email.\n". 
+"User Message: $message.\n";
+
+$to = "solteszkerigabor@outlook.hu"
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact.html");
+
+
+?>
